0b3bfc18a5fbaf2495c9257015b8d7aa4c6a1f49
Kirill <developerpopov@yandex.ru>
2021-02-05 13:59:25 +0300

    Сделал компоненту для рендера страницы с характеристиками отеля

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Main from '../main/main';
+import PlaceProperty from '../place-property/place-property';
 
-const App = ({offersCount, places}) => {
+const App = ({offersCount, places, auth, userName}) => {
   return (
-    <Main offersCount={offersCount} places={places}/>
+    <>
+      <Main offersCount={offersCount} auth={auth} userName={userName} places={places} />
+      <PlaceProperty auth={auth} userName={userName} />
+    </>
   );
 };
 
 App.propTypes = {
   offersCount: PropTypes.number.isRequired,
-  places: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired
+  places: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  auth: PropTypes.bool.isRequired,
+  userName: PropTypes.string.isRequired
 };
 
 export default App;
