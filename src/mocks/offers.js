025797a8b4baaed8708a59b3b4d21c91e21f1741
Kirill <developerpopov@yandex.ru>
2021-02-09 16:36:03 +0300

    Подготовил моковые данные для доступных предложений по аренде

---
+++ src/mocks/offers.js
@@ -0,0 +1,139 @@
+export const offers = [
+  {
+    "bedrooms": 3,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": `Amsterdam`
+    },
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    "host": {
+      "avatar_url": `img/avatar-angelina.jpg`,
+      "id": 1,
+      "is_pro": true,
+      "name": `Angelina`
+    },
+    "id": 1,
+    "images": [`img/apartment-01.jpg`, `img/apartment-02.jpg`],
+    "is_favorite": false,
+    "is_premium": false,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "max_adults": 4,
+    "preview_image": `img/apartment-01.jpg`,
+    "price": 120,
+    "rating": 4.8,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `apartment`
+  },
+  {
+    "bedrooms": 4,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": `Amsterdam`
+    },
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`],
+    "host": {
+      "avatar_url": `img/avatar-angelina.jpg`,
+      "id": 2,
+      "is_pro": true,
+      "name": `Angelina`
+    },
+    "id": 2,
+    "images": [`img/room.jpg`],
+    "is_favorite": false,
+    "is_premium": false,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "max_adults": 4,
+    "preview_image": `img/room.jpg`,
+    "price": 100,
+    "rating": 3.8,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `Private room`
+  },
+  {
+    "bedrooms": 2,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": `Amsterdam`
+    },
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`],
+    "host": {
+      "avatar_url": `img/avatar-angelina.jpg`,
+      "id": 3,
+      "is_pro": true,
+      "name": `Angelina`
+    },
+    "id": 3,
+    "images": [`img/studio-01.jpg`],
+    "is_favorite": false,
+    "is_premium": false,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "max_adults": 4,
+    "preview_image": `img/studio-01.jpg`,
+    "price": 150,
+    "rating": 5,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `Studio`
+  },
+  {
+    "bedrooms": 5,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": `Amsterdam`
+    },
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`],
+    "host": {
+      "avatar_url": `img/avatar-angelina.jpg`,
+      "id": 4,
+      "is_pro": true,
+      "name": `Angelina`
+    },
+    "id": 4,
+    "images": [`img/studio-01.jpg`],
+    "is_favorite": false,
+    "is_premium": false,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "max_adults": 4,
+    "preview_image": `img/studio-01.jpg`,
+    "price": 250,
+    "rating": 5,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `Studio`
+  },
+]
+;
---
+++ src/mocks/reviews.js
@@ -0,0 +1,51 @@
+export const reviews = [
+  {
+    "comment": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "date": `2019-05-08T14:13:56.569Z`,
+    "id": 1,
+    "rating": 4,
+    "user": {
+      "avatar_url": `img/avatar-max.jpg`,
+      "id": 1,
+      "is_pro": false,
+      "name": `Max`
+    }
+  },
+  {
+    "comment": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "date": `2019-05-08T14:13:56.569Z`,
+    "id": 2,
+    "rating": 3,
+    "user": {
+      "avatar_url": `img/avatar-max.jpg`,
+      "id": 2,
+      "is_pro": false,
+      "name": `Max`
+    }
+  },
+  {
+    "comment": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "date": `2019-05-08T14:13:56.569Z`,
+    "id": 3,
+    "rating": 2,
+    "user": {
+      "avatar_url": `img/avatar-max.jpg`,
+      "id": 3,
+      "is_pro": false,
+      "name": `Max`
+    }
+  },
+  {
+    "comment": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "date": `2019-05-08T14:13:56.569Z`,
+    "id": 4,
+    "rating": 1,
+    "user": {
+      "avatar_url": `img/avatar-max.jpg`,
+      "id": 4,
+      "is_pro": false,
+      "name": `Max`
+    }
+  },
+]
+;
