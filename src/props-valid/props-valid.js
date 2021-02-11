1cf93ea1da980940e6644744e14475b43872bc9f
Kirill <developerpopov@yandex.ru>
2021-02-11 15:46:44 +0300

    Поправил валидацию пропсов

--- src/props-valid/props-valid.js
+++ src/props-valid/props-valid.js
@@ -1,50 +1,45 @@
 import PropTypes from 'prop-types';
 
 export const offersPropValid = {
-  offers: PropTypes.arrayOf(
-      PropTypes.shape({
-        bedrooms: PropTypes.number.isRequired,
-        city: PropTypes.shape({
-          location: PropTypes.objectOf(PropTypes.number).isRequired,
-          name: PropTypes.string.isRequired
-        }).isRequired,
-        description: PropTypes.string.isRequired,
-        goods: PropTypes.arrayOf(PropTypes.string).isRequired,
-        host: PropTypes.shape({
-          avatarUrl: PropTypes.string.isRequired,
-          id: PropTypes.number.isRequired,
-          isPro: PropTypes.bool.isRequired,
-          name: PropTypes.string.isRequired,
-        }).isRequired,
-        id: PropTypes.number.isRequired,
-        images: PropTypes.arrayOf(PropTypes.string).isRequired,
-        isFavorite: PropTypes.bool.isRequired,
-        isPremium: PropTypes.bool.isRequired,
-        location: PropTypes.objectOf(PropTypes.number).isRequired,
-        maxAdults: PropTypes.number.isRequired,
-        previewImage: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-        rating: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-      }).isRequired
-  ).isRequired
+  bedrooms: PropTypes.number.isRequired,
+  city: PropTypes.shape({
+    location: PropTypes.objectOf(PropTypes.number).isRequired,
+    name: PropTypes.string.isRequired
+  }).isRequired,
+  description: PropTypes.string.isRequired,
+  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
+  host: PropTypes.shape({
+    avatarUrl: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    isPro: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  id: PropTypes.number.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  isFavorite: PropTypes.bool.isRequired,
+  isPremium: PropTypes.bool.isRequired,
+  location: PropTypes.objectOf(PropTypes.number).isRequired,
+  maxAdults: PropTypes.number.isRequired,
+  previewImage: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
-export const reviewsPropValid = {
-  reviews: PropTypes.arrayOf(
-      PropTypes.shape({
-        comment: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
+
+export const reviewsPropValid = PropTypes.arrayOf(
+    PropTypes.shape({
+      comment: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      user: PropTypes.shape({
+        avatarUrl: PropTypes.string.isRequired,
         id: PropTypes.number.isRequired,
-        rating: PropTypes.number.isRequired,
-        user: PropTypes.shape({
-          avatarUrl: PropTypes.string.isRequired,
-          id: PropTypes.number.isRequired,
-          isPro: PropTypes.bool.isRequired,
-          name: PropTypes.string.isRequired,
-        }).isRequired,
-      }).isRequired
-  ).isRequired
-};
+        isPro: PropTypes.bool.isRequired,
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired
+).isRequired;
 
