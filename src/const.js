62604da121018251392809c5fae9ec9a2b3aa8b8
Kirill <developerpopov@yandex.ru>
2021-02-13 17:55:46 +0300

    изменил путь

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
 export const Routes = {
   MAIN: `/`,
   MAIN_EMPTY: `/dev-main-empty`,
-  OFFER: `/offer:id?`,
+  OFFER: `/offer/:id`,
   LOGIN: `/login`,
   FAVOR: `/favorites`,
   FAVOR_EMPTY: `/dev-favorites-empty`
