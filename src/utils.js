f62608f8c76334eecebeb6085d2698fea5e85834
Kirill <developerpopov@yandex.ru>
2021-02-13 17:57:01 +0300

    Написал небольшую функцию для получения пути до страницы с предложением

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,9 @@
-import {STAR_LIST} from "./const";
+import {Routes, STAR_LIST} from "./const";
 
 export const getRatingCount = (rating) => (
   100 / STAR_LIST.length * rating
 );
+
+export const getOfferPath = (id) => Routes.OFFER.replace(/id/, id);
+
+
