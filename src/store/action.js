92993b3751ef4aa4640f3faa8f0c5e94054c2c0d
Kirill <developerpopov@yandex.ru>
2021-02-21 17:14:39 +0300

    Описал редьюсер и типы действий

---
+++ src/store/action.js
@@ -0,0 +1,5 @@
+export const ActionType = {
+  CHANGE_CITY: `changeCity`,
+  GET_OFFERS: `getOffers`
+}
+;
---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+import {offers} from "../mocks/offers";
+import {ActionType} from "./action";
+
+const initialState = {
+  city: `Paris`,
+  offers
+};
+
+const getOffers = (city) => (
+  offers.filter((offer) => offer.city.name === city)
+);
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_CITY:
+      return {
+        ...state,
+        city: action.payload
+      };
+    case ActionType.GET_OFFERS:
+      return {
+        ...state,
+        offers: getOffers(state.city)
+      };
+    default:
+      return state;
+  }
+};
