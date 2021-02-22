3f776f0293af2401c8c360a26079d3b9a3203bd6
Kirill <developerpopov@yandex.ru>
2021-02-22 12:12:15 +0300

    Описал actionCreator  и подключил store к списку городов

--- src/components/main/locations/locations.jsx
+++ src/components/main/locations/locations.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {CITY_LIST} from '../../../const';
+import {ActionCreator} from '../../../store/action';
 
-const Locations = () => (
-  <section className="locations container">
-    <ul className="locations__list tabs__list">
-      {
-        CITY_LIST.map((city) =>
-          <li key={city} className={`locations__item` /* tabs__item--active Этот класс отвечает за активную ссылку в меню. Его реализацию добавлю позже*/}>
-            <a className="locations__item-link tabs__item" href="#">
-              <span>{city}</span>
-            </a>
-          </li>)
-      }
-    </ul>
-  </section>
-);
+const Locations = ({changeCity}) => {
+  const menuClickHandler = (evt) => {
+    if (evt.target.tagName === `SPAN`) {
+      changeCity(evt.target.textContent);
+    }
+  };
+  return (
+    <section className="locations container">
+      <ul className="locations__list tabs__list" onClick={(evt) => menuClickHandler(evt)}>
+        {CITY_LIST.map((city) => <li key={city} className="locations__item">
+          <a className={`locations__item-link tabs__item ${city === `Paris` ? `tabs__item--active` : ``}`} href="#">
+            <span>{city}</span>
+          </a>
+        </li>)}
+      </ul>
+    </section>
+  );
+};
 
-export default Locations;
+const mapDispatchToProps = (dispatch) => ({
+  changeCity(city) {
+    dispatch(ActionCreator.changeCity(city));
+  },
+});
+
+export {Locations};
+export default connect(null, mapDispatchToProps)(Locations);
--- src/store/action.js
+++ src/store/action.js
@@ -1,5 +1,14 @@
 export const ActionType = {
   CHANGE_CITY: `changeCity`,
   GET_OFFERS: `getOffers`
-}
-;
+};
+
+export const ActionCreator = {
+  changeCity: (city) => ({
+    type: ActionType.CHANGE_CITY,
+    payload: city
+  }),
+  getOffers: () => ({
+    type: ActionType.GET_OFFERS
+  })
+};
