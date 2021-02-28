4930b14effcb168f487709968ea0a896295bafb5
Kirill <developerpopov@yandex.ru>
2021-02-28 12:50:29 +0300

    Написал асинхронные действия

---
+++ src/store/api-actions.js
@@ -0,0 +1,22 @@
+import {ActionCreator} from "./action";
+import {AuthorizationStatus} from './../const';
+
+export const fetchOfferList = () => (dispatch, _getState, api) => (
+  api.get(`/hotels`)
+  .then(({data}) => dispatch(ActionCreator.loadOffers(data)))
+);
+
+export const checkAuth = () => (dispatch, _getState, api) => (
+  api.get(`/login`)
+    .then(() =>
+      dispatch(ActionCreator.requiredAuthorization(AuthorizationStatus.AUTH))
+    )
+  .catch(()=> {})
+);
+
+export const login = ({login: email, password}) => (dispatch, _getState, api) => (
+  api.post(`/login`, {email, password})
+    .then(() =>
+      dispatch(ActionCreator.requiredAuthorization(AuthorizationStatus.AUTH))
+    )
+);
