fc5b1d2b0dfa9725a69c7133ed6eaf894b3db4fd
Kirill <developerpopov@yandex.ru>
2021-02-28 13:44:51 +0300

    Подключил адаптер

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,9 +1,10 @@
 import {ActionCreator} from "./action";
 import {AuthorizationStatus} from './../const';
+import {adaptToClient} from "./adapters";
 
 export const fetchOfferList = () => (dispatch, _getState, api) => (
   api.get(`/hotels`)
-  .then(({data}) => dispatch(ActionCreator.loadOffers(data)))
+  .then(({data}) => dispatch(ActionCreator.loadOffers(data.map((offer)=> adaptToClient(offer)))))
 );
 
 export const checkAuth = () => (dispatch, _getState, api) => (
