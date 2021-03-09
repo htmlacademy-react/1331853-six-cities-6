14560ad1e9fd966c42df3357cb17a170feab2f27
Kirill <developerpopov@yandex.ru>
2021-03-09 21:48:47 +0300

    Сделал показ списка сортировки по клику

--- src/components/main/sort/sort.jsx
+++ src/components/main/sort/sort.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {connect} from 'react-redux';
 
 import {PropTypes} from 'prop-types';
 import {ActionCreator} from '../../../store/action';
-import {SORT_LIST} from '../../../const';
+import {SORT_LIST, SORT_TEXTS} from '../../../const';
 
 const Sort = ({changeSort, currentSort}) => {
 
-  const sortClickHandler = (evt) => {
+  const selectRef = useRef();
+
+  const sortTypeClickHandler = (evt) => {
     changeSort(evt.currentTarget.dataset.sortType);
   };
+
+  const selectClickHandler = () => {
+    selectRef.current.classList.toggle(`places__options--opened`);
+  };
+
   return (
-    <ul className="places__options places__options--custom places__options--opened">
-      {
-        SORT_LIST.map(({text, type}) => <li key={type} className={`places__option ${currentSort === type ? `places__option--active` : ``}`}
-          data-sort-type={type} tabIndex={0} onClick={(evt)=> sortClickHandler(evt)}>{text}</li>)
-      }
-    </ul>
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption" >Sort by </span>
+      <span className="places__sorting-type" tabIndex={0} onClick={() => selectClickHandler()}>
+        {SORT_TEXTS[currentSort]}
+        <svg className="places__sorting-arrow" width={7} height={4}>
+          <use xlinkHref="#icon-arrow-select" />
+        </svg>
+      </span>
+      <ul className="places__options places__options--custom" ref={selectRef}>
+        {
+          SORT_LIST.map(({text, type}) => (
+            <li key={type} className={`places__option ${currentSort === type ? `places__option--active` : ``}`} data-sort-type={type} tabIndex={0} onClick={(evt) => sortTypeClickHandler(evt)}>
+              {text}
+            </li>
+          ))
+        }
+      </ul>
+    </form>
   );
 };
 
