9db75c5bdeedaf0c08b7f32e3a594bac5f9a724b
Kirill <developerpopov@yandex.ru>
2021-03-10 17:14:54 +0300

    Установил RTK и заново сконфигурировал хранилище

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {applyMiddleware, createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+
+import {configureStore} from '@reduxjs/toolkit';
+
 import App from './components/app/app';
 import rootReducer from './store/root-reducer';
 import {createApi} from './services/api';
 import {requiredAuthorization} from './store/action';
 import {AuthorizationStatus} from './const';
-import thunk from 'redux-thunk';
 import {checkAuth} from './store/api-actions';
 import {redirect} from './store/middlewares/redirect';
 
@@ -18,14 +18,16 @@ const api = createApi(
       store.dispatch(requiredAuthorization(AuthorizationStatus.NO_AUTH))
 );
 
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api
+      },
+    }).concat(redirect)
+});
 
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(
-        applyMiddleware(thunk.withExtraArgument(api)),
-        applyMiddleware(redirect)
-    )
-);
 
 store.dispatch(checkAuth());
 
