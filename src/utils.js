774cc654c2423bdcd55429663cfad3c625081cba
Kirill <developerpopov@yandex.ru>
2021-03-11 17:40:37 +0300

    Удалил более ненужную функцию

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import {Routes, SORT_TYPES, STAR_LIST} from "./const";
+import {Routes, STAR_LIST} from "./const";
 
 export const getRatingCount = (rating) => {
   const roundedRating = Math.round(rating);
@@ -13,16 +13,3 @@ export const getOfferPath = (id) => Routes.OFFER.replace(/id/, id);
 export const getCurrentOffers = (city, offers) => (
   offers.filter((offer) => offer.city.name === city)
 );
-
-export const getSortedOffers = (currentSort, offers) => {
-  switch (currentSort) {
-    case SORT_TYPES.LOW_PRICE:
-      return offers.sort((a, b) => a.price - b.price);
-    case SORT_TYPES.HIGH_PRICE:
-      return offers.sort((a, b) => b.price - a.price);
-    case SORT_TYPES.TOP_RATED:
-      return offers.sort((a, b) => b.rating - a.rating);
-    default:
-      return offers;
-  }
-};
