6e22254ec41388f720e87abf09ead2553bd202c4
Kirill <developerpopov@yandex.ru>
2021-03-12 13:17:41 +0300

    Удалил комментарий

--- src/store/user/user.js
+++ src/store/user/user.js
@@ -27,35 +27,5 @@ const user = createReducer(initialState, (builder) => {
   });
 });
 
-// const user1 = (state = initialState, action) => {
-//   switch (action.type) {
-//     // case ActionType.REQUIRED_AUTHORIZATION:
-//     //   return {
-//     //     ...state,
-//     //     authorizationStatus: action.payload
-//     //   };
-
-//     // case ActionType.CHANGE_USER_NAME:
-//     //   return {
-//     //     ...state,
-//     //     userName: action.payload
-//     //   };
-
-//     // case ActionType.CHANGE_USER_AVATAR:
-//     //   return {
-//     //     ...state,
-//     //     avatarUrl: action.payload
-//     //   };
-
-//     // case ActionType.SET_ERROR_MESSAGE:
-//     //   return {
-//     //     ...state,
-//     //     errorMessage: String(action.payload)
-//     //   };
-
-//     default:
-//       return state;
-//   }
-// };
 
 export {user};
